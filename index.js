555ff7b7c90fc89bd9a06ef80b76d96dadb44b4b
Reagan Cline <reaganecline@gmail.com>
2018-06-29 12:36:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,3 +23,13 @@ document.addEventListener('keydown', function(e) {
     moveDodgerLeft()
   }
 })
+
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 39) {
+    var leftNumbers = dodger.style.left.replace('px', '')
+    var left = parseInt(leftNumbers, 10)
+ 
+    dodger.style.left = `${left + 1}px`
+  }
+})
